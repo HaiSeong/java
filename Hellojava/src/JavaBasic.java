9a7798d2b39e3e32d5fd922e625389a011da182f
haiseong <wjdgotjd9908@gmail.com>
2022-09-15 13:39:16 +0900

    exam

---
+++ Hellojava/src/JavaBasic.java
@@ -0,0 +1,10 @@
+
+public class JavaBasic {
+
+	// application entry point
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!1");
+	}
+
+}
